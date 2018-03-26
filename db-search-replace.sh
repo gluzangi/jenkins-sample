4650c86fea51eb27660a3376c92415f6f42b82b6
Gerald Luzangi <gerald.luzangi@we.org>
2018-03-26 13:14:43 -0400

    fix the issue with CREATE DATABASE line in a dump file

--- db-search-replace.sh
+++ db-search-replace.sh
@@ -5,6 +5,7 @@
 # sed -i 's/www\.we\.org/stagingaz\.we\.org/g' db-weorg-live-search-replaced-01.sql
 #############################
 
+sed -i 's/db\_wesites\_dev/db\_wesites\_poc/g' $1
 sed -i 's/www\.we\.org/stagingaz\.we\.org/g' $1
 sed -i 's/www\.metowe\.com/stagingaz\.metowe\.com/g' $1
 sed -i 's/www\.wecharity\.org/stagingaz\.wecharity\.org/g' $1
