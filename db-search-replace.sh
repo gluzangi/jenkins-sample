3793ed004ed3e4af085c8d27cf10aa620a9d3b43
DevOps Admin <devops.admin@we.org>
2018-07-24 21:43:47 +0000

    fix the azure deployment into pre-prod

--- db-search-replace.sh
+++ db-search-replace.sh
@@ -5,10 +5,12 @@
 # sed -i 's/www\.we\.org/stagingaz\.we\.org/g' db-weorg-live-search-replaced-01.sql
 #############################
 
-sed -i 's/db\_wesites\_dev/db\_wesites\_poc/g' $1
-sed -i 's/www\.we\.org/stagingaz\.we\.org/g' $1
-sed -i 's/www\.metowe\.com/stagingaz\.metowe\.com/g' $1
-sed -i 's/www\.wecharity\.org/stagingaz\.wecharity\.org/g' $1
-sed -i 's/www\.we\.ca/stagingaz\.we\.ca/g' $1
-sed -i 's/staging\.we\.ca/stagingaz\.we\.ca/g' $1
-sed -i 's/www\.mouvementunis\.org/stagingaz\.mouvementunis\.org/g' $1
+#sed -i 's/db\_wesites\_dev/db\_wesites\_poc/g' $1
+#sed -i 's/www\.we\.org/stagingaz\.we\.org/g' $1
+#sed -i 's/www\.metowe\.com/stagingaz\.metowe\.com/g' $1
+#sed -i 's/www\.wecharity\.org/stagingaz\.wecharity\.org/g' $1
+#sed -i 's/www\.we\.ca/stagingaz\.we\.ca/g' $1
+#sed -i 's/staging\.we\.ca/stagingaz\.we\.ca/g' $1
+#sed -i 's/www\.mouvementunis\.org/stagingaz\.mouvementunis\.org/g' $1
+#
+sed -i 's/staging\.we\.ca/www\.we\.ca/g' $1
