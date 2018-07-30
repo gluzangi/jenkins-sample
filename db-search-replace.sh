e89740d9492fa277b0c266464e9479d5396f58fe
Gerald Luzangi <gerald.luzangi@we.org>
2018-07-30 16:32:34 -0400

    use stagingaz with a containerized WP code

--- db-search-replace.sh
+++ db-search-replace.sh
@@ -7,11 +7,11 @@
 
 sed -i 's/db\_wesites\_dev/db\_wesites\_poc/g' $1
 
-#sed -i 's/www\.we\.org/stagingaz\.we\.org/g' $1
-#sed -i 's/www\.metowe\.com/stagingaz\.metowe\.com/g' $1
-#sed -i 's/www\.wecharity\.org/stagingaz\.wecharity\.org/g' $1
-#sed -i 's/www\.we\.ca/stagingaz\.we\.ca/g' $1
-#sed -i 's/staging\.we\.ca/stagingaz\.we\.ca/g' $1
-#sed -i 's/www\.mouvementunis\.org/stagingaz\.mouvementunis\.org/g' $1
-#
-sed -i 's/staging\.we\.ca/www\.we\.ca/g' $1
+sed -i 's/www\.we\.org/stagingaz\.we\.org/g' $1
+sed -i 's/www\.metowe\.com/stagingaz\.metowe\.com/g' $1
+sed -i 's/www\.wecharity\.org/stagingaz\.wecharity\.org/g' $1
+sed -i 's/www\.we\.ca/stagingaz\.we\.ca/g' $1
+sed -i 's/staging\.we\.ca/stagingaz\.we\.ca/g' $1
+sed -i 's/www\.mouvementunis\.org/stagingaz\.mouvementunis\.org/g' $1
+
+#sed -i 's/staging\.we\.ca/www\.we\.ca/g' $1
